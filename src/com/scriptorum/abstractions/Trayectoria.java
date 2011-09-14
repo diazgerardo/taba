c496d89a71380be540250a2af73c1d993d7585c6
gerardo.diaz <gerardo.diaz@gmail.com>
2011-09-14 03:34:01 +0000

    Head

---
+++ src/com/scriptorum/abstractions/Trayectoria.java
@@ -0,0 +1,77 @@
+package com.scriptorum.abstractions;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import android.location.Location;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.scriptorum.singletons.Movil;
+import com.scriptorum.xml.TrayectoriaSerializer;
+
+public class Trayectoria extends Grafo implements OnClickListener, PropertyChangeListener {
+	private static final long serialVersionUID = -5433997207358050284L;
+	protected static LinkedList<Vertice> trayectoria;
+	private static BigDecimal distanciaTotalRecorrida;
+	private static Trayectoria _instance;
+	
+	private Trayectoria() {
+		trayectoria = new LinkedList<Vertice>();
+		distanciaTotalRecorrida = new BigDecimal(0);
+	}
+	
+	public static Trayectoria getInstance() {
+		if(_instance == null) {
+			_instance = new Trayectoria();
+		}
+		return _instance;
+	}
+
+	@Override
+	public void update() {
+		Movil movil = Movil.getInstance();
+		Vertice locationAnterior, locationInicial;
+		Vertice currentLocation = movil.getVertice();
+		try {
+			locationAnterior = trayectoria.getLast();
+			locationInicial = trayectoria.getFirst();
+		} catch (NoSuchElementException e) {
+			// Ups, primera vez! 
+			trayectoria.add(currentLocation);
+			return;
+		}
+		
+		float results[] = { 0, 0, 0 };
+		Location.distanceBetween(currentLocation.getLatitud().doubleValue(),currentLocation.getLongitud().doubleValue(), locationAnterior.getLatitud().doubleValue(),locationAnterior.getLongitud().doubleValue(), results);
+		BigDecimal distanciaCubierta = new BigDecimal(results[0]);
+		BigDecimal tiempoEmpleado = new BigDecimal(currentLocation.getCreacion().getTime() - locationAnterior.getCreacion().getTime());
+		BigDecimal tiempoTotalEmpleado = new BigDecimal((long)currentLocation.getCreacion().getTime() - locationInicial.getCreacion().getTime());
+		distanciaTotalRecorrida = distanciaTotalRecorrida.add(distanciaCubierta);
+		if(tiempoEmpleado.compareTo(new BigDecimal(0))!= 0)
+			movil.setVelocidadPromedioParcial(new BigDecimal((distanciaCubierta.doubleValue()/tiempoEmpleado.doubleValue())*3600));
+		if(tiempoTotalEmpleado.compareTo(new BigDecimal(0))!= 0)
+			movil.setVelocidadPromedioReal(new BigDecimal((distanciaTotalRecorrida.doubleValue()/tiempoTotalEmpleado.doubleValue())*3600));
+		trayectoria.add(currentLocation);
+	}
+	
+	public void reset() {
+		_instance = null;
+	}
+	
+	public void save() {
+		TrayectoriaSerializer.save(trayectoria);
+	}
+
+	public void onClick(View v) {
+		save();
+		reset();
+	}
+
+	public void propertyChange(PropertyChangeEvent event) {
+		update();
+	}
+}
---
+++ src/com/scriptorum/view/ThirdTab.java
@@ -0,0 +1,50 @@
+package com.scriptorum.view;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.scriptorum.abstractions.Trayectoria;
+import com.scriptorum.singletons.Movil;
+
+public class ThirdTab extends Activity {
+	static Drawable oldDrawable = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabs3);
+		OnClickListener onClickListener = new OnClickListener() {
+			
+			public void onClick(View v) {
+	
+				if(null==oldDrawable) {
+					oldDrawable = v.getBackground();
+					v.setBackgroundColor(Color.BLUE);
+				} else {
+					v.setBackgroundDrawable(oldDrawable);
+					oldDrawable = null;
+				}
+				
+			}
+		};
+		Button button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(Trayectoria.getInstance());
+		Button button2 = (Button) findViewById(R.id.button2);
+		button2.setOnClickListener(Movil.getInstance());
+		Button button3 = (Button) findViewById(R.id.button3);
+		button3.setOnClickListener(onClickListener);
+		Button button4 = (Button) findViewById(R.id.button4);
+		button4.setOnClickListener(onClickListener);
+		Button button5 = (Button) findViewById(R.id.button5);
+		button5.setOnClickListener(onClickListener);
+		Button button6 = (Button) findViewById(R.id.button6);
+		button6.setOnClickListener(onClickListener);
+
+	}
+	
+}
