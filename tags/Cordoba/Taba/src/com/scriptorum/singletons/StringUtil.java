e235b5028d1c567784e4443fc2ef090e385462bb
gerardo.diaz@gmail.com <gerardo.diaz@gmail.com@7f3bdd9c-800d-e475-527a-8b9a09abaf91>
2011-09-14 11:04:05 +0000

    detalles de release en roadmap.txt

---
+++ tags/Cordoba/Taba/src/com/scriptorum/singletons/StringUtil.java
@@ -0,0 +1,10 @@
+package com.scriptorum.singletons;
+
+public class StringUtil {
+
+	public static String strZero(long mm) {
+		String result = "00".concat(String.valueOf(mm));
+		return result.substring(result.length()-2, result.length());
+	}
+
+}
