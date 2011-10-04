d5efaaa8fccc9e1bcd6173e57c97012d483e2c29
gerardo.diaz@gmail.com <gerardo.diaz@gmail.com@7f3bdd9c-800d-e475-527a-8b9a09abaf91>
2011-10-04 04:02:52 +0000

    added capability to send SMS Messages to MovilService

--- branches/Tandil/Taba/src/ar/com/scriptorum/taba/services/MovilService.java
+++ branches/Tandil/Taba/src/ar/com/scriptorum/taba/services/MovilService.java
@@ -1,5 +1,25 @@
 package ar.com.scriptorum.taba.services;
 
+import android.app.PendingIntent;
+import android.telephony.SmsManager;
+
 public class MovilService extends AbstractService {
 
+	SmsManager smsManager = SmsManager.getDefault();
+	
+	public boolean sendNews(String news) {
+		// TODO this method will be intended to periodically send news messages
+		// -regarding travel progress, like position, time spent, remaining time
+		// to travel completion, remaining distance, whatever- to configured 
+		// telephones
+
+		String destinationAddress = "destinationAddress";
+		String scAddress = "scAddress";
+		PendingIntent deliveryIntent = null;
+		PendingIntent sentIntent = null;
+		smsManager.sendTextMessage(destinationAddress, scAddress, news, sentIntent, deliveryIntent);
+		
+		return true;
+		
+	}
 }
