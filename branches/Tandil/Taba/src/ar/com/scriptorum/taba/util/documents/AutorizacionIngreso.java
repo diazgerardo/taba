24d1be52654dad26b681cd11f77e2f106809c772
gerardo.diaz@gmail.com <gerardo.diaz@gmail.com@7f3bdd9c-800d-e475-527a-8b9a09abaf91>
2011-12-19 20:40:56 +0000

    working towards a workflow

--- branches/Tandil/Taba/src/ar/com/scriptorum/taba/util/documents/AutorizacionIngreso.java
+++ branches/Tandil/Taba/src/ar/com/scriptorum/taba/util/documents/AutorizacionIngreso.java
@@ -1,37 +1,12 @@
 package ar.com.scriptorum.taba.util.documents;
 
-public class AutorizacionIngreso implements BasicDocument {
-
-	private String department;
-	private String docName;
+public class AutorizacionIngreso extends AbstractDocument {
 
 	public AutorizacionIngreso() {
-		this.docName = Constants.AUTORIZACION_DE_INGRESO;
+		setDocName(Constants.AUTORIZACION_DE_INGRESO);
 	}
 
-	@Override
-	public String getDepartment() {
-		return this.department;
+	public void accept(DocumentVisitor v) {
+		v.visit(this);
 	}
-
-	@Override
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	@Override
-	public void accept(DocumentVisitor dv) {
-		dv.visit(this);
-	}
-
-	@Override
-	public String getDocName() {
-		return this.docName;
-	}
-
-	@Override
-	public void setDocName(String docName) {
-		this.docName = docName;
-	}
-
 }
