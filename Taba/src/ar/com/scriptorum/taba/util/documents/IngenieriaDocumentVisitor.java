acad2ff649691601ba1d0e41db6d39c9c727e4a9
gerardo.diaz <gerardo.diaz@gmail.com>
2011-12-20 14:12:41 +0000

    trying to get built an android project as if it were an standard java project

--- Taba/src/ar/com/scriptorum/taba/util/documents/IngenieriaDocumentVisitor.java
+++ Taba/src/ar/com/scriptorum/taba/util/documents/IngenieriaDocumentVisitor.java
@@ -2,12 +2,10 @@ package ar.com.scriptorum.taba.util.documents;
 
 public class IngenieriaDocumentVisitor implements DocumentVisitor {
 
-	@Override
 	public void visit(OrdenTrabajo f) {
 		f.setDepartment(Constants.INGENIERIA);
 	}
 
-	@Override
 	public void visit(AutorizacionIngreso r) {
 		r.setDepartment(Constants.INGENIERIA);
 	}
