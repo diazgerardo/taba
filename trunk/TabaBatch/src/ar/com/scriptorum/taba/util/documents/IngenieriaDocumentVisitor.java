a01376ce7b7ce1fb5313824e94e964886e85cea2
gerardo.diaz@gmail.com <gerardo.diaz@gmail.com@7f3bdd9c-800d-e475-527a-8b9a09abaf91>
2011-12-21 03:45:55 +0000

    Share project "TabaBatch" into "https://taba.googlecode.com/svn"

--- trunk/TabaBatch/src/ar/com/scriptorum/taba/util/documents/IngenieriaDocumentVisitor.java
+++ trunk/TabaBatch/src/ar/com/scriptorum/taba/util/documents/IngenieriaDocumentVisitor.java
@@ -1,13 +1,20 @@
 package ar.com.scriptorum.taba.util.documents;
 
+import ar.com.scriptorum.taba.workflows.IngenieriaDocWkfl;
+
 public class IngenieriaDocumentVisitor implements DocumentVisitor {
 
-	public void visit(OrdenTrabajo f) {
-		f.setDepartment(Constants.INGENIERIA);
+	public void visit(OrdenTrabajo ot) {
+		commonBehaviour((AbstractDocument) ot);
 	}
 
-	public void visit(AutorizacionIngreso r) {
-		r.setDepartment(Constants.INGENIERIA);
+	public void visit(AutorizacionIngreso ai) {
+		commonBehaviour((AbstractDocument) ai);
+	}
+	
+	private void commonBehaviour(AbstractDocument d) {
+		d.setDepartment(Constants.INGENIERIA);
+		d.setWorkflow(new IngenieriaDocWkfl());
 	}
 
 }
