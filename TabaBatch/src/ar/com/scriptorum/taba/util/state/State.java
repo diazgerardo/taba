2913d13b8ad23e966019250207bdb7b31b911ab9
gerardo.diaz <gerardo.diaz@gmail.com>
2011-12-24 00:30:03 +0000

    DocumentTransitionTest seemed to be working but there were a problem in condition handling. fixed.

--- TabaBatch/src/ar/com/scriptorum/taba/util/state/State.java
+++ TabaBatch/src/ar/com/scriptorum/taba/util/state/State.java
@@ -1,14 +1,22 @@
 package ar.com.scriptorum.taba.util.state;
 
 public class State extends NullState {
-    public State(String state) {
-		this.state=state;
-	}
 
 	String state;
 	
+	public State(String state) {
+		this.state=state;
+	}
+	
 	public String toString() {
 		return state;
+	}
+	
+	public boolean equals(Object o) {
+		if( null == o ) return false;
+		if( !(o instanceof State) ) return false;
+		State state = (State)o;
+		return this.state.equals(state.toString());
 	}
 }
 
--- TabaBatch/test/ar/com/scriptorum/taba/util/documents/DocumentTransitionTest.java
+++ TabaBatch/test/ar/com/scriptorum/taba/util/documents/DocumentTransitionTest.java
@@ -1,5 +1,8 @@
 package ar.com.scriptorum.taba.util.documents;
 
+import ar.com.scriptorum.taba.factories.StateFactory;
+import ar.com.scriptorum.taba.workflows.IngenieriaDocWkfl;
+
 public class DocumentTransitionTest extends DocumentConfiguratorsTest {
 
 	public DocumentTransitionTest(String test) {
@@ -7,9 +10,18 @@ public class DocumentTransitionTest extends DocumentConfiguratorsTest {
 	}
 
 	public void testAutorizacionIngresoTransition() {
+
+		assertNotNull(aiIng.getWorkflow());
+		assertTrue(aiIng.getWorkflow() instanceof IngenieriaDocWkfl);
+		assertNotNull(aiIng.getWorkflow().getCurrentState());
+		assertTrue(StateFactory.newState("edited").equals(aiIng.getWorkflow().getCurrentState()));
 		assertTrue(aiIng.getWorkflow().transicionate());
+		assertTrue(StateFactory.newState("checked").equals(aiIng.getWorkflow().getCurrentState()));
 		assertTrue(aiIng.getWorkflow().transicionate());
+		assertTrue(StateFactory.newState("validated").equals(aiIng.getWorkflow().getCurrentState()));
 		assertTrue(aiIng.getWorkflow().transicionate());
+		assertTrue(StateFactory.newState("finalized").equals(aiIng.getWorkflow().getCurrentState()));
+		
 	}
 
 }
